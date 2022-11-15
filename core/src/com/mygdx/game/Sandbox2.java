36b5ba3577ec143de83554cdc124346f2d7ea1c7
tmoodini <tom.moody@gmail.com>
2022-11-15 15:31:20 -0700

    First Commit

---
+++ core/src/com/mygdx/game/Sandbox2.java
@@ -0,0 +1,86 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class Sandbox2 extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Skin skin;
+	TextButton b1;
+	Stage stage;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		
+		skin = new Skin(Gdx.files.internal("glassy/glassy-ui.json"));
+		
+		b1 = new TextButton("B1",skin);
+		 stage = new Stage(new ScreenViewport());
+		b1.setSize(100, 100);
+        b1.setOrigin(0, 0);
+        b1.setX(stage.getViewport().getScreenWidth()/2);
+        b1.setY(stage.getViewport().getScreenHeight()/2);
+        
+        
+        
+        b1.addListener(new InputListener() {
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log("my app", "Pressed"); //** Usually used to start Game, etc. **//
+                b1.setText("Pressed");
+                batch.begin();
+                batch.draw(img,0,0);
+                batch.end();
+                
+                return true;
+        }
+       
+        public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log("my app", "Released");
+                b1.setText("B1");
+                
+        }
+    });
+       
+        stage.act();
+		 stage.addActor(b1);
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 0);
+		
+	     
+        
+        Gdx.input.setInputProcessor(stage);
+        
+        
+       
+		//batch.draw(img, 0, 0);
+		 
+		    stage.draw();
+		    
+		batch.begin();    
+		//b1.draw(batch, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
