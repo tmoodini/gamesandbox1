80ba7c04d9c18539ee15fa7bc797ff1dd25c0395
tmoodini <tom.moody@gmail.com>
2023-01-24 14:34:05 -0700

    Updated

--- core/src/com/mygdx/game/Sandbox2.java
+++ core/src/com/mygdx/game/Sandbox2.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Sandbox2 extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -20,16 +21,22 @@ public class Sandbox2 extends ApplicationAdapter {
 	Skin skin;
 	TextButton b1;
 	Stage stage;
+	Viewport viewport;
 	
 	@Override
 	public void create () {
+		
+		
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		
 		skin = new Skin(Gdx.files.internal("glassy/glassy-ui.json"));
 		
+		
+		viewport = new ScreenViewport();
 		b1 = new TextButton("B1",skin);
-		 stage = new Stage(new ScreenViewport());
+		 stage = new Stage(viewport);
+		 //Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		b1.setSize(100, 100);
         //b1.setOrigin(-50, -50);
         b1.setPosition(-50, 0);
@@ -42,11 +49,12 @@ public class Sandbox2 extends ApplicationAdapter {
        
         stage.act();
 		 stage.addActor(b1);
+		 //Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, 0, 0);
+		ScreenUtils.clear(1, 1, 1, 1);
 		
 	     
         
@@ -55,12 +63,18 @@ public class Sandbox2 extends ApplicationAdapter {
         
        
 		//batch.draw(img, 0, 0);
-		 
+		 stage.getViewport().update(stage.getViewport().getScreenWidth(), stage.getViewport().getScreenHeight());
 		    stage.draw();
-		    
+		    stage.act();
 		batch.begin();    
 		//b1.draw(batch, 0);
 		batch.end();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		
+		viewport.update(width, height);
 	}
 	
 	@Override
@@ -68,4 +82,6 @@ public class Sandbox2 extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+	
+	
 }
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Sandbox2");
+		config.setWindowedMode(800, 800);
 		new Lwjgl3Application(new Sandbox2(), config);
 	}
 }
